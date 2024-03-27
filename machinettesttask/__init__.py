2a2f387bc33aeed4e7319f034186fa6ab495e9da
Egor251 <egorpyshny@gmail.com>
2024-03-27 09:22:10 +0300

    add ci/cd

--- machinettesttask/__init__.py
+++ machinettesttask/__init__.py
@@ -2,16 +2,6 @@ import random
 import string
 
 
-def error_handler(func):
-    def wrapper(*args, **kwargs):
-        try:
-            func(*args, **kwargs)
-        except Exception:
-            pass
-
-    return wrapper
-
-
 def generate_random_file_name():
     length = 15
     random_chars = random.choices(string.ascii_letters + string.digits, k=length)
--- machinettesttask/app.py
+++ machinettesttask/app.py
@@ -1,7 +1,9 @@
+import os
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-from machinettesttask import error_handler, generate_random_file_name
+from machinettesttask import generate_random_file_name
 
 app = Flask(__name__)
 socketio = SocketIO(app, max_http_buffer_size=1024 * 1024 * 5)
@@ -22,30 +24,44 @@ def handle_disconnect():
     print("Client disconnected")
 
 
-@error_handler
 @socketio.on("file_upload")
 def handle_file_upload(data):
-    with open("files\\" + generate_random_file_name(), "w") as file:
-        for d in data:
-            filename: str = d["name"]
-            file_req = d["request"]
-            if not filename.endswith(".md") and not filename.endswith(".txt"):
+    file_name = generate_random_file_name()
+    while os.path.exists(file_name):
+        file_name = generate_random_file_name()
+    with open(f"files\\{file_name}", "w") as file:
+        try:
+            for d in data:
+                filename: str = d["name"]
+                file_req = d["request"]
+                if not filename.endswith(".md") and not filename.endswith(".txt"):
+                    emit(
+                        "file_upload_status",
+                        {
+                            "message": f"File {filename} "
+                            f"have incorrect extension REQUEST NUM {file_req}"
+                        },
+                    )
+                    continue
+                file_content = d["fileData"]
+                file.write(file_content)
+                file.write("\n<file-separator>\n")
                 emit(
                     "file_upload_status",
-                    {
-                        "message": f"File {filename} "
-                        f"have incorrect extension REQUEST NUM {file_req}"
-                    },
+                    {"message": f"File {filename} uploaded REQUEST NUM {file_req}"},
                 )
-                continue
-            file_content = d["fileData"]
-            file.write(file_content)
-            file.write("\n<file-separator>\n")
+        except Exception as e:
             emit(
                 "file_upload_status",
-                {"message": f"File {filename} uploaded REQUEST NUM {file_req}"},
+                {"message": f"Error during handling {filename}, {e.args[0]}"},
             )
 
 
 if __name__ == "__main__":
+    directory_path = f"{os.path.abspath('.')}\\files"
+    if not os.path.exists(directory_path) or not os.path.isdir(directory_path):
+        os.makedirs(directory_path)
+    if not os.path.exists(directory_path+".gitignore"):
+        with open('files\\.gitignore', 'w') as gitignore_file:
+            gitignore_file.write("*\n!.gitignore")
     socketio.run(app, allow_unsafe_werkzeug=True)
